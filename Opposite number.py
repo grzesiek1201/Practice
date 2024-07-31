8c5d87d092e49b20c2ab06631df9b681b8048002
Grzegorz <138705337+grzesiek1201@users.noreply.github.com>
2024-07-31 17:52:31 +0200

    Create Opposite number.py

---
+++ Opposite number.py
@@ -0,0 +1,12 @@
+def opposite(number):
+    return number - 2*number
+
+
+"""Description:
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34"""
