0cde9289cb9806301897ac4b0853949631cf9fbe
Grzegorz <138705337+grzesiek1201@users.noreply.github.com>
2024-07-31 17:48:27 +0200

    Create Reversed Strings.py

---
+++ Reversed Strings.py
@@ -0,0 +1,9 @@
+def solution(string):
+    return string[::-1]
+
+
+"""Description:
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'"""
