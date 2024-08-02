18fd23bed711c487b25b994c1d7eb9badd7f6f8d
Grzegorz <138705337+grzesiek1201@users.noreply.github.com>
2024-08-02 12:57:40 +0200

    Create  Convert a Boolean to a String.py

---
+++ Convert a Boolean to a String.py
@@ -0,0 +1,8 @@
+def boolean_to_string(b):
+    a = str(b)
+    return a
+
+
+"""mplement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given."""
