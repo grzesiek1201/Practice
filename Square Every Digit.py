a82bc4706eb03c39671d5413e4a895b4e1b9c117
Grzegorz <138705337+grzesiek1201@users.noreply.github.com>
2024-08-05 11:36:28 +0200

    Update Square Every Digit.py

--- Square Every Digit.py
+++ Square Every Digit.py
@@ -1,8 +1,8 @@
 def square_digits(num):
     result = ""
     for n in str(num):
-        sq= int(n) **2
-        sum += str(sq)
+        sq = int(n) ** 2
+        result += str(sq)
     return int(result)
     
       
